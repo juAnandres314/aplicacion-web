ff3324b644e13537c3b2dd815a23bce3d81059d5
juAnandres314 <juanandrespizo@iemmmrip.edu.co>
2024-09-03 12:03:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var ne=Math.floor(Math.random() * (100 - 1) + 1);
+function adivinar(){
+    var nd=document.getElementById("barra").value;
+    if(ne==nd){
+        alert("ganaste");
+    }else if(ne>nd){
+        alert("el número es mayor");
+    }else{
+        alert("el número es menor");
+    }
+}
